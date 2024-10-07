dc7e9f5702711f85ea7333282730184fb3ae452a
PlacyG <114609499+PlacyG@users.noreply.github.com>
2024-10-07 15:20:00 +0200

    Add files via upload

---
+++ dwa.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formularz wyslany</title>
+</head>
+<body>
+   <?php
+   
+   
+   
+   
+   $im = $_GET['imie'];
+   $kot = $_GET['Kotek'];
+   $rasa = $_GET['rasa'];
+   $loze = $_GET['lozko'];
+   
+   
+   
+   echo $im. "<br/>";
+   echo $kot. "<br/>";
+   echo $rasa. "<br/>";
+   echo $loze. "<br/>";
+   
+   
+
+
+
+
+   ?>
+
+
+</body>
+</html>
